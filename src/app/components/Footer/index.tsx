68f963d7a1405527b772aed0786ae1052a463b53
Abdallah Hemedah <hemedah94@gmail.com>
2020-05-14 13:38:05 +0200

    :sparkles: feat: add footer component

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,28 @@
+/**
+ *
+ * Footer
+ *
+ */
+import React from 'react';
+import styled from 'styled-components/macro';
+import { useTranslation } from 'react-i18next';
+
+import { Layout } from 'antd';
+
+interface Props {}
+
+export function Footer(props: Props) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { t } = useTranslation();
+
+  return <GlobalFooter>{'Tabib © 2020 Created by ABZO'}</GlobalFooter>;
+}
+
+const GlobalFooter = styled(Layout.Footer)`
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: none;
+`;
